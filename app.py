b92e0240acf075e9201bf2ec2e2b3a909929d268
Fitriannisaa Nabila <nabila.fitri494@gmail.com>
2025-11-20 15:11:10 +0700

    Memperbaiki error di syntax

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 pages = [
     st.Page(page="pages/page1.py", title="Home", icon="🏡"),
-    st.Page(page="pages/page2.py", title="visualisasi Data", icon="📊"),
+    st.Page(page="pages/page2.py", title="Visualisasi Data", icon="📊"),
     st.Page(page="pages/page3.py", title="Settings", icon="⚙️")
 ]
 
